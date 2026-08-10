0f1f05d278477df4e6c37519c5319a5be50079cf
senayit <senaitaweke2019@gmail.com>
2026-08-10 05:52:43 +0300

    fix issues again

--- src/lib/orders/fetchAllOrders.ts
+++ src/lib/orders/fetchAllOrders.ts
@@ -1,14 +1,12 @@
 import { orders } from "./placeholderdata";
-import { CalcuDataPages } from "../calcuDataPages";
 export async function fetchOrders() {
   try {
     const ordersData = orders;
-    const totalPages = CalcuDataPages(orders);
 
     // console.log(`ordersData is :${ordersData}`);
     // console.log(`orders total pages is is :${totalPages}`);
 
-    return { orders: ordersData, total: totalPages };
+    return { orders: ordersData };
   } catch (err) {
     console.error("Database Error:", err);
     throw new Error("Failed to fetch all orders.");
